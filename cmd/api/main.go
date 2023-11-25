6fed081c0d988f5ccfb865e5829ac6097016532a
RobertoSuarez <electrosonix12@gmail.com>
2023-11-25 19:52:04 +0000

    ya se puede registra el usuario

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"github.com/RobertoSuarez/art-digital/data"
 	"github.com/RobertoSuarez/art-digital/db"
+	"github.com/RobertoSuarez/art-digital/handlers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/viper"
 )
 
 func main() {
+
+	viperApp := viper.New()
+
+	viperApp.AutomaticEnv()
+
 	app := fiber.New()
 
 	db.Init()
@@ -17,5 +24,25 @@ func main() {
 		&data.User{},
 	)
 
-	app.Listen(":3000")
+	db.DB.Where(&data.Country{Name: "Ecuador"}).
+		FirstOrCreate(&data.Country{
+			Name:          "Ecuador",
+			Code:          "EC",
+			ContinentName: "America",
+		})
+
+	api := app.Group("/api")
+
+	users := api.Group("/users")
+
+	userController := handlers.NewUserController(viperApp)
+
+	// registrar un usuario
+	users.Post("/", userController.HandlerRegisterUser)
+
+	users.Post("/login", func(c *fiber.Ctx) error {
+		return c.SendString("login correcto")
+	})
+
+	app.Listen(":4000")
 }
--- handlers/users_handlers.go
+++ handlers/users_handlers.go
@@ -1,9 +1,38 @@
 package handlers
 
-type UserHandler struct{}
+import (
+	"github.com/RobertoSuarez/art-digital/data"
+	"github.com/RobertoSuarez/art-digital/types"
+	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/viper"
+)
 
-func NewUserHandler() *UserHandler {
-	return &UserHandler{}
+type UserController struct {
+	viper *viper.Viper
 }
 
-// metodos de esta estructura
+func NewUserController(viper *viper.Viper) *UserController {
+	return &UserController{
+		viper: viper,
+	}
+}
+
+func (userc *UserController) HandlerRegisterUser(c *fiber.Ctx) error {
+
+	var apiUser types.UserAPI
+	if err := c.BodyParser(&apiUser); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	err := data.RegisterUser(&apiUser)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(apiUser)
+
+}
